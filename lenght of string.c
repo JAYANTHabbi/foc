bc007950c79e482aafbab7a13035e6471778c9ea
JAYANTHabbi <123968320+JAYANTHabbi@users.noreply.github.com>
2024-02-26 12:31:42 +0530

    Add files via upload

---
+++ lenght of string.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+	char str[20];
+	printf("enter the string:");
+	gets (str);
+	printf("the lenght of the string is :%d",strlen(str));
+	getch();
+}
